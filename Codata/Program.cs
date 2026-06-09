7d88987ea1133a7fb8dde6aab423d84cb44e2d9b
ren <ericren39@gmail.com>
2026-06-09 22:11:55 +0200

    create files

---
+++ Codata/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows.Forms;
+using Codata.scripts;
+
+class MainForm : Form
+{
+    public event Action OnEnterKeyDown;
+    public event Action<string> OnKeyDown;
+
+    public ListBox  ListBox;
+    public TextBox textBox;
+    public MainForm()
+    {
+        this.Text = "Codata";
+        this.Width = 800;
+        this.Height = 600;
+
+        TextBox textBox = this.textBox = new TextBox();
+        textBox.Location = new System.Drawing.Point(50, 50);
+        textBox.Width = 200;
+
+        Button button = new Button();
+        button.Text = "enter";
+        button.Location = new System.Drawing.Point(50, 100);
+        void click()
+        {
+            Program.command.Command(textBox.Text);
+            MessageBox.Show("输出：" + Program.output);
+            Program.output = "";
+        }
+        button.Click += (s, e) => click();
+        OnEnterKeyDown += click;
+        textBox.KeyDown += (s, e) =>
+        {
+            OnKeyDown?.Invoke(e.KeyCode.ToString());
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                Console.WriteLine("enter");
+                OnEnterKeyDown?.Invoke();
+            }
+        };
+
+        Controls.Add(textBox);
+        Controls.Add(button);
+        this.KeyPreview = true;
+
+        //list
+        ListBox listBox = ListBox = new ListBox();
+        listBox.Location = new System.Drawing.Point(400, 50);
+        listBox.Width = 200;
+        listBox.Height = 150;  // 超过高度就会自动出现滚动条
+
+        this.Controls.Add(listBox);
+    }
+    private void Keydown(object sender, KeyEventArgs e)
+    {
+        OnKeyDown?.Invoke(e.KeyCode.ToString());
+
+        if (e.KeyCode == Keys.Enter)
+        {
+            Console.WriteLine("enter");
+            OnEnterKeyDown?.Invoke();
+        }
+    }
+}
+
+
+class Program
+{
+    private static string author = "WindSearched";
+    public static CommandBranch command;
+    public static string output = "";
+    public static MainForm form;
+
+    public static string addOutput
+    {
+        set => output = value;
+    }
+
+    [STAThread]
+    static void Main()
+    {
+        RegisterCommands();
+
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        form = new MainForm();
+        form.OnKeyDown += downed =>
+        {
+            var s = form.textBox.Text + downed;
+            var l = s.Split(' ');
+            form.ListBox.Items.Clear();
+            form.ListBox.DataSource = command.GetSuggestions(l.ToList());;
+        };
+
+        Application.Run(form);
+    }
+
+    static void RegisterCommands()
+    {
+        command = new CommandBranch("codata")
+                .AddBranch(new CommandBranch("author")
+                    .Execute(_ =>
+                    {
+                        addOutput = author;
+                        return true;
+                    }))
+                .AddBranch(new CommandBranch("add")
+                    .AddArguments(new("a"),new("b"))
+                    .Execute(arg =>
+                    {
+                        if(float.TryParse(arg.Get("a"), out float a))
+                            if (float.TryParse(arg.Get("b"), out float b))
+                            {
+                                addOutput = (a + b).ToString();
+                            }
+                        return false;
+                    }))
+                .AddBranch(new CommandBranch("print")
+                    .AddArgument(new CommandBranch.Argument("name")
+                        .SetSuggestion(() => new List<string>
+                        {
+                            "wind","searched", "helloworld", "ciao", "hi", "114514"
+                        })))
+            ;
+    }
+}
---
+++ Codata/scripts/CommandBranch.cs
@@ -0,0 +1,169 @@
+﻿namespace Codata.scripts;
+
+public class CommandBranch
+{
+    public string name;
+    public Func<CommandArg, bool> execute;
+    public Func<List<string>> suggestion;
+    public List<CommandBranch> branches = new List<CommandBranch>();
+    public List<Argument> arguments = new List<Argument>();
+
+    public CommandBranch AddBranch(CommandBranch branch)
+    {
+        branches.Add(branch);
+        return this;
+    }
+
+    public CommandBranch AddBranches(params CommandBranch[] branches)
+    {
+        foreach (var branch in branches)
+            AddBranch(branch);
+        return this;
+    }
+
+    public CommandBranch AddArgument(Argument argument)
+    {
+        arguments.Add(argument);
+        return this;
+    }
+
+    public CommandBranch AddArguments(params Argument[] arguments)
+    {
+        foreach (var argument in arguments)
+            AddArgument(argument);
+        return this;
+    }
+
+    /// <summary>
+    /// Add the execute method of this command branch
+    /// </summary>
+    /// <param name="execute"></param>
+    /// <returns></returns>
+    public CommandBranch Execute(Func<CommandArg, bool> execute)
+    {
+        this.execute = execute;
+        return this;
+    }
+
+    public CommandBranch SetSuggestion(Func<List<string>> suggestion)
+    {
+        this.suggestion = suggestion;
+        return this;
+    }
+
+    public CommandBranch Parse(List<string> splited, out CommandArg args)
+    {
+        if (splited.Count == 0)
+        {
+            args = null;
+            return this;
+        }
+
+        var s = splited[0];//get the first value
+        foreach (var branch in branches.Where(branch => branch.name == s))
+        {   // parse if is existed branch named s
+            splited.RemoveAt(0);
+            return branch.Parse(splited, out args);
+        }
+
+        if (splited.Count != arguments.Count)
+        {
+            args = null;
+            return this;
+        }
+
+        args = new CommandArg();
+        for (int i = 0; i < arguments.Count; i++)
+        {
+            var arg = arguments[i];
+            args.SetArg(arg.argument, splited[i]);
+        }
+        return this;
+    }
+
+    public CommandBranch Parse(List<string> splited, out int remainsCount)
+    {
+        if (splited.Count == 0)
+        {
+            remainsCount = 0;
+            return this;
+        }
+
+        var s = splited[0];//get the first value
+        foreach (var branch in branches.Where(branch => branch.name == s))
+        {   // parse if is existed branch named s
+            splited.RemoveAt(0);
+            return branch.Parse(splited, out remainsCount);
+        }
+
+        remainsCount = splited.Count;
+        return this;
+    }
+
+    public bool Command(string arg)
+    {
+        var list = arg.Split(' ').ToList();
+        var branch = Parse(list, out CommandArg args);
+        return branch.execute != null && branch.execute.Invoke(args);
+    }
+
+    public delegate List<string> CMDSuggestion();
+
+    public CommandBranch(string name)
+    {
+        this.name = name;
+    }
+
+    public override string ToString() => name;
+
+    public List<string> GetSuggestions(List<string> args)
+    {
+        var last = args.Last();
+
+        var b = Parse(args, out int i);
+
+        if (b.arguments[i].suggestion != null)
+        {
+            var l = b.arguments[i].suggestion.Invoke();
+            return l.Where(v => v.StartsWith(last)).ToList();
+        }
+
+        return null;
+    }
+
+    public class Argument
+    {
+        public string argument;
+        public Func<List<string>> suggestion;
+
+        public Argument(string argument)
+        {
+            this.argument = argument;
+        }
+
+        public Argument SetSuggestion(Func<List<string>> suggestion)
+        {
+            this.suggestion = suggestion;
+            return this;
+        }
+    }
+    public class CommandArg
+    {
+        public Dictionary<string, string> args = new();
+
+        public string Get(string key) => args.ContainsKey(key) ? args[key] : null;
+
+        public string Get(int index)
+        {
+            var a = args.Values.ToList();
+            return a.Count > index ? a[index] : null;
+        }
+
+        public void SetArg(string key, string value, bool overwrite = true)
+        {
+            if (args.TryAdd(key, value)) return;
+            if(overwrite)
+                args[key] = value;
+        }
+    }
+}
