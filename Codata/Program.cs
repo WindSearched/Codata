45ec16c4b33ee1da9bd81ab1e9fb0106e68a9f87
ren <ericren39@gmail.com>
2026-06-10 23:30:18 +0200

    now can use suggestion(testing)

--- Codata/Program.cs
+++ Codata/Program.cs
@@ -4,125 +4,149 @@ using Codata.scripts;
 
 class MainForm : Form
 {
-    public event Action OnEnterKeyDown;
-    public event Action<string> OnKeyDown;
+	public event Action OnEnterKeyDown;
+	public event Action<string> OnKeyDown;
+	public event Action OnTabDown;
 
-    public ListBox  ListBox;
-    public TextBox textBox;
-    public MainForm()
-    {
-        this.Text = "Codata";
-        this.Width = 800;
-        this.Height = 600;
+	public ListBox  ListBox;
+	public TextBox textBox;
+	public MainForm()
+	{
+		this.Text = "Codata";
+		this.Width = 800;
+		this.Height = 600;
 
-        TextBox textBox = this.textBox = new TextBox();
-        textBox.Location = new System.Drawing.Point(50, 50);
-        textBox.Width = 200;
+		TextBox textBox = this.textBox = new TextBox();
+		textBox.Location = new System.Drawing.Point(50, 50);
+		textBox.Width = 200;
 
-        Button button = new Button();
-        button.Text = "enter";
-        button.Location = new System.Drawing.Point(50, 100);
-        void click()
-        {
-            Program.command.Command(textBox.Text);
-            MessageBox.Show("输出：" + Program.output);
-            Program.output = "";
-        }
-        button.Click += (s, e) => click();
-        OnEnterKeyDown += click;
-        textBox.KeyDown += (s, e) =>
-        {
-            OnKeyDown?.Invoke(e.KeyCode.ToString());
+		Button button = new Button();
+		button.Text = "enter";
+		button.Location = new System.Drawing.Point(50, 100);
+		void click()
+		{
+			Program.command.Command(textBox.Text);
+			MessageBox.Show("输出：" + Program.output);
+			Program.output = "";
+		}
+		button.Click += (s, e) => click();
+		OnEnterKeyDown += click;
+		KeyDown += Keydown;
 
-            if (e.KeyCode == Keys.Enter)
-            {
-                Console.WriteLine("enter");
-                OnEnterKeyDown?.Invoke();
-            }
-        };
+		Controls.Add(textBox);
+		Controls.Add(button);
+		this.KeyPreview = true;
 
-        Controls.Add(textBox);
-        Controls.Add(button);
-        this.KeyPreview = true;
+		//list
+		ListBox listBox = ListBox = new ListBox();
+		listBox.Location = new System.Drawing.Point(400, 50);
+		listBox.Width = 200;
+		listBox.Height = 150;  // 超过高度就会自动出现滚动条
 
-        //list
-        ListBox listBox = ListBox = new ListBox();
-        listBox.Location = new System.Drawing.Point(400, 50);
-        listBox.Width = 200;
-        listBox.Height = 150;  // 超过高度就会自动出现滚动条
+		this.Controls.Add(listBox);
+	}
+	private void Keydown(object sender, KeyEventArgs e)
+	{
+		OnKeyDown?.Invoke(e.KeyCode.ToString());
 
-        this.Controls.Add(listBox);
-    }
-    private void Keydown(object sender, KeyEventArgs e)
-    {
-        OnKeyDown?.Invoke(e.KeyCode.ToString());
-
-        if (e.KeyCode == Keys.Enter)
-        {
-            Console.WriteLine("enter");
-            OnEnterKeyDown?.Invoke();
-        }
-    }
+		if (e.KeyCode == Keys.Enter)
+		{
+			Console.WriteLine("enter");
+			OnEnterKeyDown?.Invoke();
+		}
+		else if (e.KeyCode == (Keys)18)//temporanealty
+		{
+			Console.WriteLine("insert");
+			OnTabDown?.Invoke();
+		}
+	}
 }
 
 
 class Program
 {
-    private static string author = "WindSearched";
-    public static CommandBranch command;
-    public static string output = "";
-    public static MainForm form;
+	private static string author = "WindSearched";
+	public static CommandBranch command;
+	public static string output = "";
+	public static MainForm form;
 
-    public static string addOutput
-    {
-        set => output = value;
-    }
+	public static string addOutput
+	{
+		set => output = value;
+	}
 
-    [STAThread]
-    static void Main()
-    {
-        RegisterCommands();
+	[STAThread]
+	static void Main()
+	{
+		RegisterCommands();
 
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        form = new MainForm();
-        form.textBox.TextChanged += (_,_) =>
-        {
-            var s = form.textBox.Text;
-            var l = s.Split(' ');
-            form.ListBox.DataSource = command.GetSuggestions(l.ToList());;
-        };
+		Application.EnableVisualStyles();
+		Application.SetCompatibleTextRenderingDefault(false);
+		form = new MainForm();
+		form.textBox.TextChanged += (_,_) =>
+		{
+			var s = form.textBox.Text;
+			var l = s.Split(' ');
+			form.ListBox.DataSource = command.GetSuggestions(l.ToList());;
+		};
 
-        Application.Run(form);
-    }
+		BindingKeys();
 
-    static void RegisterCommands()
-    {
-        command = new CommandBranch("codata")
-                .SetSuggestion(b =>  b.branches.Select(v => v.name).ToList())
-                .AddBranch(new CommandBranch("author")
-                    .Execute(_ =>
-                    {
-                        addOutput = author;
-                        return true;
-                    }))
-                .AddBranch(new CommandBranch("add")
-                    .AddArguments(new("a"),new("b"))
-                    .Execute(arg =>
-                    {
-                        if(float.TryParse(arg.Get("a"), out float a))
-                            if (float.TryParse(arg.Get("b"), out float b))
-                            {
-                                addOutput = (a + b).ToString();
-                            }
-                        return false;
-                    }))
-                .AddBranch(new CommandBranch("print")
-                    .AddArgument(new CommandBranch.Argument("name")
-                        .SetSuggestion(() => new List<string>
-                        {
-                            "wind","searched", "helloworld", "ciao", "hi", "114514"
-                        })))
-            ;
-    }
+		Application.Run(form);
+	}
+
+	static void RegisterCommands()
+	{
+		command = new CommandBranch("codata")
+				.SetSuggestion(b =>  b.branches.Select(v => v.name).ToList())
+				.AddBranch(new CommandBranch("author")
+					.Execute(_ =>
+					{
+						addOutput = author;
+						return true;
+					}))
+				.AddBranch(new CommandBranch("add")
+					.AddArguments(new("a"),new("b"))
+					.Execute(arg =>
+					{
+						if(float.TryParse(arg.Get("a"), out float a))
+							if (float.TryParse(arg.Get("b"), out float b))
+							{
+								addOutput = (a + b).ToString();
+							}
+						return false;
+					}))
+				.AddBranch(new CommandBranch("print")
+					.AddArgument(new CommandBranch.Argument("name")
+						.SetSuggestion(() => new List<string>
+						{
+							"wind","searched", "helloworld", "ciao", "hi", "114514"
+						}))
+					.Execute(arg =>
+					{
+						output = arg.Get("name");
+						return true;
+					}))
+			;
+	}
+
+	static void BindingKeys()
+	{
+		form.OnTabDown += () =>
+		{
+			Console.WriteLine("OnTabDown");
+			var box = form.ListBox;
+			if(box.SelectedItem == null) return;
+			var s = box.SelectedItem.ToString();
+
+			var c = form.textBox.Text;
+			int index = c.LastIndexOf(' ');
+
+			c = index != -1 ? c.Substring(0, index+1) : "";
+			c += ' ' + s;
+			form.textBox.Text = c;
+			form.textBox.SelectionStart = form.textBox.Text.Length;
+			form.textBox.SelectionLength = 0;
+		};
+	}
 }
