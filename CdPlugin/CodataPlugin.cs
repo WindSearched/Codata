5a1428c8a939b8d59c8005f8875711fb4c0f5d29
ren <ericren39@gmail.com>
2026-08-30 15:15:59 +0200

    Add plugin interface

---
+++ CdPlugin/CodataPlugin.cs
@@ -0,0 +1,23 @@
+﻿namespace CdPlugin;
+
+public interface CodataPlugin
+{
+    public void OnStart()
+    {
+
+    }
+    public void OnClose()
+    {
+
+    }
+
+    public string GetAuthor()
+    {
+        return "WS";
+    }
+
+    public string GetName()
+    {
+        return "Codata";
+    }
+}
