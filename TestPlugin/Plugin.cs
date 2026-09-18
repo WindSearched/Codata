a8bb9ec33cad5c11732b916f1f8c2e91cc22a932
ren <ericren39@gmail.com>
2026-09-18 22:39:31 +0200

    Add a exeample plugin

---
+++ TestPlugin/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using CdPlugin;
+
+namespace TestPlugin;
+
+public class Plugin : CdInterface
+{
+    public CommandBranch[] GetCommands() => [];
+
+    public void OnStart()
+    {
+        Center.Print("TestPlugin Loaded");
+    }
+
+    public void OnClose()
+    {
+        Center.Print("TestPlugin Closed");
+
+    }
+}
